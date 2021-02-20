5daf923d82a12259f98aec54201e6b46d2721253
kuri-sauce <thekarinoelle@gmail.com>
2021-02-20 02:16:20 -0800

    Update about.html style.css index.html js/jquery script.js watchguide.html
    2.3 updates
    
    added jquery compressed file and min script
    
    mynavtoggle id and navtoggle class

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+
+
 console.log("Hello world! Who wants a high five?")
+
+var age = 39;
+var name = "Kari"
+var personParent = {
+  name: name,
+  age: age,
+}
+
+console.log(personParent, "I enjoy binge watching my life away whilst eating bon bons.")
