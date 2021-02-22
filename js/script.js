6981fc5d097908df0fd708b6f41fef5c80dff627
kuri-sauce <thekarinoelle@gmail.com>
2021-02-22 03:21:30 -0800

    Update 2.3 and animation
    created toggle menu
    changed animation to bobbing head
    added logos folder with svgs
    changed social svgs to file sources

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 console.log("Hello world! Who wants a high five?")
 
 var age = 39;
@@ -10,3 +8,12 @@ var personParent = {
 }
 
 console.log(personParent, "I enjoy binge watching my life away whilst eating bon bons.")
+
+function menuToggle() { //What is menuToggle? where is this coming from? is this coming from jQuery?//
+  var x = document.getElementById('myNavtoggle'); //Is .getElementById from the library too?//
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
