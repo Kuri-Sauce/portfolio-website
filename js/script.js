b6c6c354ce9c86d5edc5d0d9222d264c651a1fa2
kuri-sauce <thekarinoelle@gmail.com>
2021-03-28 04:20:41 -0700

    script.js linter resume update copied index hero img
    updated js after running linter
    copied index to test out new hero design
    updated resume

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
-console.log("Hello world! Who wants a high five?")
+console.log('Hello world! Who wants a high five?')
 
-var age = 39;
-var name = "Kari"
+var age = 39
+var name = 'Kari'
 var personParent = {
   name: name,
-  age: age,
+  age: age
 }
 
-console.log(personParent, "WatchGuide and Tasty Vegan are currently under construction. In the meantime, please enjoy the jpg version of my case studies.");
-console.log(personParent, "I enjoy binge watching my life away whilst eating bon bons.")
+console.log(personParent, 'WatchGuide and Tasty Vegan are currently under construction. In the meantime, please enjoy the jpg version of my case studies.')
+console.log(personParent, 'I enjoy binge watching my life away whilst eating bon bons.')
 
-function menuToggle() { //What is menuToggle? where is this coming from? is this coming from jQuery?//
-  var x = document.getElementById('myNavtoggle'); //Is .getElementById from the library too?//
+// What is menuToggle? where is this coming from? is this coming from jQuery?//
+// Is .getElementById from the library too?//
+
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
